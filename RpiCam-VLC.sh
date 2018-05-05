fbf13da7642c05ae036d71ba1a74ac63cff04f25
Griff-au <paulgriffin.au@gmail.com>
2018-05-06 07:06:59 +1000

    Latest updates

--- RpiCam-VLC.sh
+++ RpiCam-VLC.sh
@@ -27,13 +27,16 @@ VLCStream ()
     local ansr=0
     local locRetVal=0
     local chceOk="n"
+    local vlcProt=""
+    local vlcPort="8554"
 
     while [ $chceOk = "n" ]; do
         tput cup $(($start_row + 3)) $left_col; echo "Choose ouput stream to use below"
         tput cup $(($start_row + 5)) $left_col; echo "1. Send output to RTSP"
         tput cup $(($start_row + 6)) $left_col; echo "2. Send output to HTTP"
         tput cup $(($start_row + 7)) $left_col; echo "3. Exit - Return to Main Menu" 
-        tput cup $(($start_row + 9)) $left_col; read -p "Enter your choice 1,2 or 3 : " ansr 
+        tput cup $(($start_row + 9)) $left_col; tput bold; read -p "Enter your choice 1,2 or 3 : " ansr; tput sgr0
+
         if [[ "$ansr" =~ ^-?[0-9]+$ ]]; then
             if [ $ansr -ge 1 ] && [ $ansr -le 3 ]; then
                 chceOk="y"
@@ -49,7 +52,13 @@ VLCStream ()
     done
 
     if [[ $ansr =~ [1|2] ]]; then
-        tput cup $(($start_row + 3)) $left_col; tput el 
+        if [ $ansr -eq 1 ]; then
+            vlcProt="rtsp"
+        else
+            vlcProt="http"
+        fi
+        tput cup $(($start_row + 3)) $left_col; echo "Use URL below to view video from within VLC"
+        tput cup $(($start_row + 4)) $left_col; echo "$vlcProt://$ipAddr:$vlcPort/"
         tput cup $(($start_row + 5)) $left_col; tput el 
         tput cup $(($start_row + 6)) $left_col; tput el 
         tput cup $(($start_row + 7)) $left_col; tput el 
@@ -65,16 +74,33 @@ VLCStream ()
 
 StartVlc ()
 {
-    local myPid=0
+    local vlcPid=0
+    local ansr=""
 
-    if [ $vlcStream == 1 ]; then
-       raspivid -o - -t 0 -n -w 600 -h 400 -fps 12 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264 2> /dev/null &
+    tput cup $(($start_row + 6)) $left_col; read -p "Start streaming VLC Video y/n : " ansr
+
+    if [ "$ansr" = "y" ]; then
+       StartVlc
     else
-       raspivid -o - -t 0 -vf |cvlc -v stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8554}' :demux=h264 2> /dev/null 
+       msg="Returning to main menu - No streaming video"; . $DisplayMsg; . $PressEnter
+    fi 
+
+    if [ "$ansr" = "y" ]; then
+        if [ $vlcStream == 1 ]; then
+            raspivid -o - -t 0 -n -w 600 -h 400 -fps 12 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264 2> /dev/null &
+        else
+            raspivid -o - -t 0 -vf |cvlc -v stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8554}' :demux=h264 2> /dev/null &
+        fi
+        vlcPid=$!
+        msg="VLC started, PID is $vlcPid."; . $DisplayMsg; . $PressEnter
+    elif [ "$ansr" = "n" ]; then
+        msg="No chosen, VLC not started, returning to main menu."; . $DisplayMsg; . $PressEnter
+    else
+        msg="VLC not started, returning to main menu."; . $DisplayMsg; . $PressEnter
     fi
 
-    myPid=$!
-    tput cup $(($start_row + 7)) $left_col; echo "$myPid started in background"
+    vlcPid=$!
+    msg="VLC started, PID is $vlcPid."; . $DisplayMsg; . $PressEnter
 }
 
 # -----------------------------------
@@ -84,6 +110,7 @@ StartVlc ()
  
 ipAddr=$(sed -e 's/^[ \t]*//;s/[ \t]*$//' <<<"$(hostname -I)")
 vlcStream=0
+ansr=""
 
 hdrLne2="        VLC Video       "
 
@@ -94,20 +121,12 @@ CheckVLC
 if [ $? -eq 0 ]; then
    VLCStream
    if [ $? -eq 0 ]; then
-      tput cup $(($start_row + 3)) $left_col; echo "Use URL below to view video from within VLC"
-      if [[ $vlcStream =~ [1|2] ]]; then
-         if [ $vlcStream == 1 ]; then
-            tput cup $(($start_row + 4)) $left_col; echo "rtsp://$ipAddr:8554/"
-         else
-            tput cup $(($start_row + 4)) $left_col; echo "http://$ipAddr:8554/"
-         fi
-         tput cup $(($start_row + 6)) $left_col; read -p "Start streaming VLC Video y/n : " ansr
-         if [ $ansr = "y" ]; then
-            StartVlc
-         else
-            msg="Returning to main menu - No streaming video"; . $DisplayMsg; . $PressEnter
-         fi 
-      fi
+      tput cup $(($start_row + 6)) $left_col; read -p "Start streaming VLC Video y/n : " ansr
+      if [ "$ansr" = "y" ]; then
+         StartVlc
+      else
+         msg="Returning to main menu - No streaming video"; . $DisplayMsg; . $PressEnter
+      fi 
    else
       msg="Returning to main menu - No Streaming Video"; . $DisplayMsg; .$PressEnter
    fi
