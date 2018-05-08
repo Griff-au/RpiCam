444281ba97f3860d70b5aa32f2d935f974e839ed
Griff-au <paulgriffin.au@gmail.com>
2018-05-08 20:44:59 +1000

    Latest update

--- RpiCam-EncTL.sh
+++ RpiCam-EncTL.sh
@@ -19,9 +19,9 @@ EncodeType ()
           choiceOk=true
           encodeType=$ansr 
           if [ $ansr = "1" ]; then
-             tlMp4="rpi_tl_"$tlDate"_full_out.mp4"
+             tlMp4="pitl_${tlDate}_nocrop.mp4"
           elif [ $ansr = "2" ]; then
-             tlMp4="rpi_tl_"$tlDate"_crop_out.mp4"
+             tlMp4="pitl_${tlDate}_crop.mp4"
           fi 
           if [[ $ansr =~ [xX] ]]; then
              locretVal=1
@@ -30,7 +30,7 @@ EncodeType ()
           tput cup $(($start_row + 11)) $left_col; echo "Choice must be 1,2,x"
        fi
     done
- 
+
     tput cup $(($start_row + 4)) $left_col; echo "TimeLapse mp4 : $tlMp4"
     tput cup $(($start_row + 6))  $left_col; tput el 
     tput cup $(($start_row + 7))  $left_col; tput el 
@@ -47,22 +47,22 @@ EncodeType ()
 EncodeTL ()
 {
     local ansr="n"
-    local myPid=0
-
-    tlOut="${piTL}/${tlMp4}"
+    local encPid=0
+    local tlOut="${piTL}/${tlMp4}"
+    local tlLog="${piTL}/tlenc_${tlDate}_${tlTime}.log"
+    local avConvCrop="-vf crop=2592:1458,scale=1280:720"
 
     tput cup $(($start_row + 6)) $left_col; echo "$numJpg jpg files found."
     tput cup $(($start_row + 7)) $left_col; read -p "Start encoding y/n : " ansr 
 
     if [ $ansr = "y" ]; then
        if [ $encodeType -eq 1 ]; then
-          nohup avconv -r 10 -i $jpgFile -r 10 -vcodec libx264 -crf 20 -g 15 $tlOut 2> /dev/null &
-#          nohup avconv -r 10 -i $jpgFile -r 10 -vcodec libx264 -crf 20 -g 15 $tlOut 
+          nohup avconv -r 10 -i $jpgFile -r 10 -vcodec libx264 -crf 20 -g 15 $tlOut &> $tlLog &
        else
-          nohup avconv -r 10 -i $jpgFile -r 10 -vcodec libx264 -crf 20 -g 15 -vf crop=2592:1458,scale=1280:720 $tlOut 2> /dev/null & 
+          nohup avconv -r 10 -i $jpgFile -r 10 -vcodec libx264 -crf 20 -g 15 $avConvCrop $tlOut &> $tlLog & 
        fi
-       myPid=$!
-       tput cup $(($start_row + 8)) $left_col; echo "$myPid started with NOHUP in background"
+       encPid=$!
+       msg="Encode started, PID is $encPid."; . $DisplayMsg; . $PressEnter
     fi
 }
 
@@ -74,6 +74,7 @@ numJpg=$(ls -l $piTL/*.jpg 2> /dev/null | wc -l)
 tlFile=$(ls $piTL/*0001.jpg 2> /dev/null)
 encodeType=0
 tlDate=""
+tlTime=""
 
 hdrLne2="    Encode Timelapse    "
 . $PiCamHdr
@@ -83,8 +84,8 @@ tput cup $(($start_row + 3)) $left_col; echo "Directory     : $piTL"
 if [ -d $piTL ]; then
    if [ $numJpg -gt 0 ]; then
       if [ ! -z $tlFile ]; then 
-         IFS='_' read part_01 part_02 tlDate part_04 <<< "$tlFile"
-         jpgFile=${piTL}"/rpi_tl_"$tlDate"_%04d.jpg"
+         IFS='-' read part_01 tlDate tlTime part_04 <<< "$tlFile"
+         jpgFile="${piTL}/PiTl-${tlDate}-${tlTime}-%04d.jpg"
          unset IFS
          EncodeType
          if [ $? -eq 0 ]; then
